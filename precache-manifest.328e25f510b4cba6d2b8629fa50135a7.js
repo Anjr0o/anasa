772a8511cde148d4befbdc914f8c5a9783cd0d95
Anjr0o <84.andrew.stokes@gmail.com>
2020-05-27 18:58:59 -0500

    Updates

---
+++ precache-manifest.328e25f510b4cba6d2b8629fa50135a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18a0e9a5dffcdbed640ad574e94bd84f",
+    "url": "/anasa/index.html"
+  },
+  {
+    "revision": "24ffde2775a93dd47e66",
+    "url": "/anasa/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4b1593160841c71d5a6e",
+    "url": "/anasa/static/js/2.5a649a2b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/anasa/static/js/2.5a649a2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24ffde2775a93dd47e66",
+    "url": "/anasa/static/js/main.66777843.chunk.js"
+  },
+  {
+    "revision": "44295d5efe7e3ad60c1a",
+    "url": "/anasa/static/js/runtime-main.203fc770.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/anasa/static/media/logo.5d5d9eef.svg"
+  }
+]);
