164b7c41d279d0eac6b6b43c9ad7b6899af2ff30
Anjr0o <84.andrew.stokes@gmail.com>
2020-05-27 20:03:59 -0500

    Once again testing deployment

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload let's try this again boiiiiiii.
+          Edit <code>src/App.js</code> and save to reload let's try this again boiiiiiii.  Once again testing
         </p>
         <a
           className="App-link"
