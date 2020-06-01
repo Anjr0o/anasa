2e60cfb2e8718815366091768a5026d5277e16f2
Anjr0o <84.andrew.stokes@gmail.com>
2020-06-01 18:26:45 -0500

    Updated strikeThrough to be fully functional

--- src/App.js
+++ src/App.js
@@ -8,17 +8,37 @@ import Contact from './Contact/Contact';
 class App extends Component {
 
   state = {
+    todos: [
+      { id: 'lekjlkck', todoName: 'getting somethin done', status: 'Finish', strikeThrough: {} },
+      { id: 'elkjx', todoName: 'getting somethin else done', status: 'Finish', strikeThrough: {} },
+      { id: 'bnenn', todoName: 'doin somethin', status: 'Finish', strikeThrough: {} }
+    ],
     status: 'Finish',
     todoName: '',
-    strikeThrough: {}
+    strikeThrough: {},
   }
 
-  checkedHandler = () => {
-    console.log('Task Completed');
-    this.setState({ status: 'Finished', strikeThrough: { textDecoration: 'line-through' } })
+  checkedHandler= (event, id) => {
+    const todoIndex = this.state.todos.findIndex(todo => {
+      return todo.id === id;
+    });
+
+    const todo = {
+      ...this.state.todos[todoIndex]
+    };
+    console.log(this.state.todos[todoIndex].todoName + ' completed');
+
+    todo.strikeThrough = {textDecoration: 'line-through'};
+    todo.status = 'Finished';
+
+    const todos = [...this.state.todos];
+    todos[todoIndex] = todo;
+
+    this.setState({ todos: todos })
   }
 
   newTodoHandler = (event) => {
+
     this.setState({
       todoName: event.target.value
     })
@@ -30,12 +50,20 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <Todos changed={this.newTodoHandler} />
-          <Todo
+          {this.state.todos.map((todo, index) => {
+            return <Todo
+              todoName={todo.todoName}
+              status={todo.status}
+              click={(event) => this.checkedHandler(event, todo.id)}
+              style={todo.strikeThrough}
+              key={todo.id} />
+          })}
+          {/* <Todo
             todoName={this.state.todoName}
             status={this.state.status}
             click={this.checkedHandler}
             style={this.state.strikeThrough}
-          />
+          /> */}
           <a
             className="App-link"
             href="https://reactjs.org"
--- src/Todos/Todos.js
+++ src/Todos/Todos.js
@@ -3,8 +3,7 @@ import React from 'react';
 let todos = (props) => {
     return (
         <div>
-            <input type="text" onChange={props.changed}/>
-            <p>I'm the todos component!</p>
+            <input type="text" placeholder="I'm the todos component!" onChange={props.changed}/>
             <div>{props.children}</div>
         </div>
     )
