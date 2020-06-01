b8c4762ed65ec70c5df702af474a5a851a3cf0f7
Anjr0o <84.andrew.stokes@gmail.com>
2020-05-31 19:33:50 -0500

    Added Todo component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Todos from './Todos/Todos';
+import Todo from './Todo/Todo';
 import Contact from './Contact/Contact';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload let's try this again boiiiiiii.  Once again testing
+class App extends Component {
+
+  state = {
+    status: 'Finish'
+  }
+
+  checkedHandler = () => {
+    console.log('Completed');
+    this.setState({status: 'Finished'})
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload let's try this again boiiiiiii.  Once again testing
         </p>
-        <Todos>Bro</Todos>
-        <Contact/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+
+          <button onClick={this.checkedHandler}>Check?</button>
+          <Todo status={this.state.status}/>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-    </div>
-  );
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Todo/Todo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+let todo = (props) => {
+    return (
+        <div>
+            <p>{props.status} getting something done</p>
+        </div>
+    )
+}
+
+export default todo;
