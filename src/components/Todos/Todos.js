d0d05d4c5ad9015b4bf630a0023ace1a985ac266
Anjr0o <84.andrew.stokes@gmail.com>
2020-06-07 19:15:13 -0500

    Updated component structure

---
+++ src/components/Todos/Todos.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Todo from './Todo/Todo';
+import FlipMove from 'react-flip-move';
+
+
+const todos = (props) =>
+    <FlipMove>{
+        props.todos.map((todo, index) => {
+            return (
+                <Todo
+                    className={todo.strikeThrough}
+                    todoName={todo.todoName}
+                    status={todo.status}
+                    click={(event) => props.checked(event, todo.id)}
+                    key={todo.id} />
+            )
+        })
+    }
+    </FlipMove>
+
+export default todos;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,50 +1,61 @@
 import React, { Component } from 'react';
 import classes from './App.module.css';
-import Todos from '../components/Todos/TodoForm';
-import Todo from '../components/Todos/Todo/Todo';
-import FlipMove from 'react-flip-move';
+import TodoForm from '../components/Todos/TodoForm';
+import Todos from '../components/Todos/Todos';
 import Contact from '../components/Contact/Contact';
 
 class App extends Component {
 
   state = {
+    // Value of TodoForm.js Input box
     value: '',
-    // Tutorial Todos
+    // Starting Todos for tutorial
     todos: [
       {
+        // Key
         id: 'kdj3',
         todoName: 'click this item',
+        // Status will change upon completion
         status: 'Try to',
+        // Strikethrough is inactive
         strikeThrough: ''
       },
       {
+        // Key
         id: '4939',
         todoName: "click this",
+        // Status will change upon completion
         status: 'Click the bottom, ',
+        // Strikethrough is already active
         strikeThrough: classes.strikethrough
       }
     ],
   }
 
+  // Upon click
   checkedHandler = (event, id) => {
+    // Find the index of the item that was clicked by searching through todos to find the key that matches it
     const todoIndex = this.state.todos.findIndex(todo => {
       return todo.id === id;
     });
 
+    // Use the index we just found to create a variable (via spread) of the todo that we found at that index
     const todo = {
       ...this.state.todos[todoIndex]
     };
 
-    console.log(this.state.todos + ' completed');
-    // updates strikethrough effect
+    // Update status and set strikethrough to active for the todo variable we just made
     todo.strikeThrough = classes.strikethrough;
-    todo.status = 'I no longer need to';
+    todo.status = 'I no longer need to ';
 
+    // Create a variable (via spread) of the todos we currently have
     const todos = [...this.state.todos];
-    console.log(todos);
+    // Add our updated todo in place of the old todo at todoIndex
     todos[todoIndex] = todo;
+    // If clicked bottom todo: delete todo
     if (todoIndex == todos.length - 1) {
       todos.splice(todoIndex, 1);
+      // If todolist is empty: create a todo item to find more todos
       if (todos.length == 0) {
         todos.push(
           {
@@ -56,7 +67,9 @@ class App extends Component {
         )
       }
     }
+    // Otherwise, move the todo to the end
     else { todos.push(todos.splice(todoIndex, 1)[0]); }
+    // Finally, set state to finished todos array
     this.setState({ todos: todos })
   }
 
@@ -76,24 +89,17 @@ class App extends Component {
       ...this.state.todos
       ]
     });
-    this.setState({value: ""});
+    this.setState({ value: "" });
     event.preventDefault();
   }
 
   render() {
     return (
       <div className={classes.App}>
-          <Todos submit={this.handleSubmit} value={this.state.value} changed={this.handleChange} />
-          <FlipMove>
-            {this.state.todos.map((todo, index) => {
-              return <Todo
-                className={todo.strikeThrough}
-                todoName={todo.todoName}
-                status={todo.status}
-                click={(event) => this.checkedHandler(event, todo.id)}
-                key={todo.id} />
-            })}
-          </FlipMove>
+        <TodoForm submit={this.handleSubmit} value={this.state.value} changed={this.handleChange} />
+        <Todos
+          todos={this.state.todos}
+          checked={this.checkedHandler} />
       </div>
     );
   }
